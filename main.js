ea81057d14278d4fb4bb2cb066b2f23480f59575
Ryan <ryanbrinklow98@gmail.com>
2021-07-22 13:01:50 +0100

    Rand Quote Gen js functionality

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const arr1 = ["If You Can Dream It", "Don't Watch the Clock", "The Best Views Come",
- "The Future Is Promised to Noone", "Success is The Best", "Failure is a Bend in The Road", "It Always Seems Impossible", "Done is", "There is No Substitute" ];
+const arr1 = ["If You Can Dream It", "Don't Watch the Clock", "The Best Views Come", "The Future Is Promised to Noone", "Success is The Best", "Failure is a Bend in The Road", "It Always Seems Impossible", "Done is", "There is No Substitute", "Smashed it"];
 
- const arr2 = ["You Can Do It", "Do What It Does, Keep Moving Forward", "After the Hardest Climb", "Go for It", "Revenge", "Not the End", "Until It Is Done", 
- "Outside of Your Comfort Zone", "Better than Perfect", "For Hard Work"];
+ const arr2 = ["You Can Do It", "Do What It Does, Keep Moving Forward", "After the Hardest Climb", "Go for It", "Revenge", "Not the End", "Until It Is Done", "Outside of Your Comfort Zone", "Better than Perfect", "For Hard Work"];
 
- const arr3 = ["Unknown", "Anonymous", "Steve Jobs", "Dwayne 'The Rock' Johnson", "Lionel Messi", "Albert Einstein", "Marie Curie", "Walt Disney", "Joe Wicks", "Stormzy"];
+ const arr3 = ["Unknown", "Anonymous", "Steve Jobs", "Dwayne Johnson", "Lionel Messi", "Albert Einstein", "Marie Curie", "Walt Disney", "Joe Wicks", "Stormzy"];
 
- 
+let quote = () => {
+    let randNum1 = Math.floor(Math.random() * 10);
+    let randNum2 = Math.floor(Math.random() * 10);
+    let randNum3 = Math.floor(Math.random() * 10);
+    return `${arr1[randNum1]}, ${arr2[randNum2]} - ${arr3[randNum3]}`
+}
+
+console.log(quote());
