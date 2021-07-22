44d0f5d53a90c569be34dec79d2f517b3258446e
Ryan <ryanbrinklow98@gmail.com>
2021-07-22 11:50:13 +0100

    Added Quotes arrays in mainjs

---
+++ main.js
@@ -0,0 +1,9 @@
+const arr1 = ["If You Can Dream It", "Don't Watch the Clock", "The Best Views Come",
+ "The Future Is Promised to Noone", "Success is The Best", "Failure is a Bend in The Road", "It Always Seems Impossible", "Done is", "There is No Substitute" ];
+
+ const arr2 = ["You Can Do It", "Do What It Does, Keep Moving Forward", "After the Hardest Climb", "Go for It", "Revenge", "Not the End", "Until It Is Done", 
+ "Outside of Your Comfort Zone", "Better than Perfect", "For Hard Work"];
+
+ const arr3 = ["Unknown", "Anonymous", "Steve Jobs", "Dwayne 'The Rock' Johnson", "Lionel Messi", "Albert Einstein", "Marie Curie", "Walt Disney", "Joe Wicks", "Stormzy"];
+
+ 
